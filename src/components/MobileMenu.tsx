bb8fbc853f723f4ea71a36ad379694b59cdac9cc
GoBDaNWeb <bogdan.ognitsiy@yandex.ru>
2022-07-29 22:53:17 +0300

    mini fix v3

---
+++ src/components/MobileMenu.tsx
@@ -0,0 +1,9 @@
+const MobileMenu = () => {
+    return (
+        <div className='fixed top-20 h-28 py-4 w-full shadow-md px-10 z-40 bg-white'>
+            
+        </div>
+    )
+}
+
+export default MobileMenu
