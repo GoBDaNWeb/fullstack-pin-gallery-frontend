99a463ad9e835642bbfe76312eed17d65e3bde29
GoBDaNWeb <bogdan.ognitsiy@yandex.ru>
2022-08-18 18:15:23 +0300

    pin v1.2.1 (fix error)

--- src/components/UI/Input/index.tsx
+++ src/components/UI/Input/index.tsx
@@ -8,7 +8,7 @@ const Input = ({func, type, placeholder, name, value}: {func: any, type: string,
             value={value}
             className='border-solid rounded-full h-10 px-2 border-[1px] border-gray-200 bg-gray-100 outline-none w-full' 
         />
-    )
+    ) 
 }
 
 export default Input
